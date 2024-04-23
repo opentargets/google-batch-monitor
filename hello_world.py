efcefa2c066ff1dc77a76805049c7a3a5eda370b
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2024-04-23 20:19:23 +0100

    2024-04-23 v3: Modifications for 5k batch

--- hello_world.py
+++ hello_world.py
@@ -8,9 +8,9 @@ import hail as hl
 batch_task_index = int(sys.argv[1])
 print(f"This is worker for task # {batch_task_index}")
 
-path_study_locus = "gs://genetics-portal-dev-analysis/yt4/toy_studdy_locus_alzheimer"
-path_si = "gs://gwas_catalog_data/study_index"
-path_out = "gs://gentropy-tmp/finemapping/v2_2024-04-23"
+path_study_locus = "gs://genetics-portal-dev-analysis/yt4/ukbb_ppp_no_mhc_small"
+path_study_index = "gs://gentropy-tmp/finemapping/ukbb_study_index_corrected2_2024-04-23"
+path_out = "gs://gentropy-tmp/finemapping/v3_2024-04-23_5k"
 
 hail_home = os.path.dirname(hl.__file__)
 session = Session(
@@ -20,22 +20,22 @@ session = Session(
         "spark.jars": "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop3-latest.jar"
     },
 )
-# hl.init(sc=session.spark.sparkContext, log="/dev/null")
 
 id_to_process = str(
     int(
         session.spark.read.parquet(path_study_locus)
+        .select("studyLocusId")
         .toPandas()
         .loc[batch_task_index, "studyLocusId"]
     )
 )
 
-L = SusieFineMapperStep(
+SusieFineMapperStep(
     session=session,
     study_locus_to_finemap=id_to_process,
     study_locus_collected_path=path_study_locus,
-    study_index_path=path_si,
+    study_index_path=path_study_index,
     output_path=path_out,
     locus_radius=100_000,
-    locus_l=10,
+    max_causal_snps=10,
 )
