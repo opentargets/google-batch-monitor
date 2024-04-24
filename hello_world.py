ecb6c81562d673cbc4d15998f862f7f6050481c6
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2024-04-24 12:26:18 +0100

    2024-04-24 v4: 1K rows, ±1MB window run

--- hello_world.py
+++ hello_world.py
@@ -8,16 +8,24 @@ import hail as hl
 batch_task_index = int(sys.argv[1])
 print(f"This is worker for task # {batch_task_index}")
 
-path_study_locus = "gs://genetics-portal-dev-analysis/yt4/ukbb_ppp_no_mhc_small"
-path_study_index = "gs://gentropy-tmp/finemapping/ukbb_study_index_corrected2_2024-04-23"
-path_out = "gs://gentropy-tmp/finemapping/v3_2024-04-23_5k"
+path_study_locus = (
+    "gs://genetics-portal-dev-analysis/yt4/ukbb_ppp_no_mhc_collected_locus_2Mb"
+)
+path_study_index = (
+    "gs://gentropy-tmp/finemapping/ukbb_study_index_corrected2_2024-04-23"
+)
+path_out = "gs://gentropy-tmp/finemapping/v4_2024-04-24_1k_2Mb"
 
 hail_home = os.path.dirname(hl.__file__)
 session = Session(
     hail_home=hail_home,
     start_hail=True,
     extended_spark_conf={
-        "spark.jars": "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop3-latest.jar"
+        "spark.jars": "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop3-latest.jar",
+        "spark.dynamicAllocation.enabled": "false",
+        "spark.driver.memory": "24g",
+        "spark.kryoserializer.buffer.max": "500m",
+        "spark.driver.maxResultSize": "3g",
     },
 )
 
@@ -36,6 +44,6 @@ SusieFineMapperStep(
     study_locus_collected_path=path_study_locus,
     study_index_path=path_study_index,
     output_path=path_out,
-    locus_radius=100_000,
+    locus_radius=1_000_000,
     max_causal_snps=10,
 )
--- runner.sh
+++ runner.sh
@@ -1,5 +1,5 @@
+set -x
 time1=$(date +%s.%N)
-
 # Mount point for the relevant Google Cloud bucket.
 export MOUNT_ROOT=/mnt/share
 # Create directory for logs, if necessary.
@@ -8,44 +8,38 @@ mkdir -p ${LOG_DIR}
 # Redirect all subsequent logs.
 # The $BATCH_TASK_INDEX variable is set by Google Batch.
 exec &> ${LOG_DIR}/${BATCH_TASK_INDEX}.log
-
 # Establish dependency installation lock.
 while ! mkdir /tmp/install.lock 2>/dev/null; do
     sleep 0.5
 done
-sudo apt -y update
 export HOME=/tmp/home
 mkdir -p $HOME
-sudo apt install bc
-sudo apt install -y curl git gcc make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libffi-dev default-jdk
-if ! [ -d "$HOME/.pyenv" ]; then
+if ! [ -d /tmp/dependencies_installed ]; then
+    sudo apt -y update
+    sudo apt install -y bc
+    sudo apt install -y curl git gcc make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libffi-dev default-jdk
     curl https://pyenv.run | bash
 fi
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init --path)"
-pyenv install 3.10.11
+if ! [ -d /tmp/dependencies_installed ]; then
+    pyenv install 3.10.11
+fi
 pyenv global 3.10.11
-echo ">>> Python version"
-python3 --version
-python3 -m ensurepip
-echo ">>> Update PIP and setuptools"
-python3 -m pip install --upgrade pip setuptools
-echo $?
-echo ">>> Install packages"
-#python3 -m pip install -r ${MOUNT_ROOT}/code/requirements.txt
-python3 -m pip install git+https://github.com/opentargets/gentropy@dev
-echo $?
+if ! [ -d /tmp/dependencies_installed ]; then
+    python3 -m ensurepip
+    python3 -m pip install --upgrade pip setuptools
+    python3 -m pip install git+https://github.com/opentargets/gentropy@dev
+fi
+# Mark dependency installation complete
+mkdir -p /tmp/dependencies_installed
 # Release dependency installation lock.
 rmdir /tmp/install.lock
 
 time2=$(date +%s.%N)
-
-echo ">>> Run script"
 python3 ${MOUNT_ROOT}/code/hello_world.py ${BATCH_TASK_INDEX}
 export RETURNCODE=$?
 
 time3=$(date +%s.%N)
-
-echo "!!! Index ${BATCH_TASK_INDEX} Overhead $(echo "$time2 - $time1" | bc) Execution $(echo "$time3 - $time2" | bc)"
-echo ">>> Completed with return code: ${RETURNCODE}"
+echo "!!! Returncode ${RETURNCODE} Index ${BATCH_TASK_INDEX} Overhead $(echo "$time2 - $time1" | bc) Execution $(echo "$time3 - $time2" | bc)"
 exit ${RETURNCODE}
